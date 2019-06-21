f84764168d2431d574ac6a2eb671ffccd7f90ebc
christopher.hogg <christopher.hogg@blik.io>
2019-06-21 17:32:35 +0100

    Updates

---
+++ precache-manifest.51039fecd79d5450ca1f7c37cbbb76ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a8071aa2d540fa1125f8da54d06eaf",
+    "url": "/game-of-flags-speech/index.html"
+  },
+  {
+    "revision": "83837a8a8e8c73bdd3f4",
+    "url": "/game-of-flags-speech/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3d66d8a29c39c29714b1",
+    "url": "/game-of-flags-speech/static/js/2.a76a9697.chunk.js"
+  },
+  {
+    "revision": "83837a8a8e8c73bdd3f4",
+    "url": "/game-of-flags-speech/static/js/main.082ca2ae.chunk.js"
+  },
+  {
+    "revision": "d7db9be0d049503bb8c8",
+    "url": "/game-of-flags-speech/static/js/runtime~main.4f77c931.js"
+  }
+]);
