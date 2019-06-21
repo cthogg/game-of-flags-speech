1f1e52ee8ebc7c86d97f836b81b9f40773e3eda1
christopher.hogg <christopher.hogg@blik.io>
2019-06-21 15:17:31 +0100

    feat: basic gameplay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import SpeechRecognition from "react-speech-recognition";
+
+const countries = {
+  "AD": {
+    "name": "Andorra",
+    "native": "Andorra",
+    "phone": "376",
+    "continent": "EU",
+    "capital": "Andorra la Vella",
+    "currency": "EUR",
+    "languages": [
+      "ca"
+    ],
+    "emoji": "🇦🇩",
+    "emojiU": "U+1F1E6 U+1F1E9"
+  },
+  "AE": {
+    "name": "United Arab Emirates",
+    "native": "دولة الإمارات العربية المتحدة",
+    "phone": "971",
+    "continent": "AS",
+    "capital": "Abu Dhabi",
+    "currency": "AED",
+    "languages": [
+      "ar"
+    ],
+    "emoji": "🇦🇪",
+    "emojiU": "U+1F1E6 U+1F1EA"
+  },
+  "AF": {
+    "name": "Afghanistan",
+    "native": "افغانستان",
+    "phone": "93",
+    "continent": "AS",
+    "capital": "Kabul",
+    "currency": "AFN",
+    "languages": [
+      "ps",
+      "uz",
+      "tk"
+    ],
+    "emoji": "🇦🇫",
+    "emojiU": "U+1F1E6 U+1F1EB"
+  }}
 
 const propTypes = {
   // Props injected by SpeechRecognition
@@ -14,26 +57,35 @@ const Dictaphone = ({
   resetTranscript,
   browserSupportsSpeechRecognition
 }) => {
+  const numberOfCountries = Object.keys(countries).length
+  const [countryNumber, setCountryNumber] = useState(0);
+  const [lastRightWrong, setLastRightWrong] = useState(false)
+  let selectedCountryObject = Object.values(countries)[countryNumber]  
+  let selectionCountryName = selectedCountryObject.name  
+  let selectionCountryEmoji = selectedCountryObject.emoji
+  if(countryNumber===numberOfCountries-1){
+    return <div> <p> End of quiz</p></div>
+  }
+  if(transcript.includes(selectionCountryName)){
+    setLastRightWrong(true)
+    setCountryNumber(countryNumber +1)
+    resetTranscript()
+  }
   if (!browserSupportsSpeechRecognition) {
-    return null;
+    return <div> <p> This site only works on Chrome </p></div>;
   }
-  const transcriptArray =transcript.split(' ')
-  const lastWordSpoken = transcriptArray[transcriptArray.length -1]
-  let correct = false
-  if(lastWordSpoken.includes('Brazil')){
-    correct = true
-  }
+
 
 
   return (
     <div>
-      {correct===true && <p> Correct </p>}
-      {correct===false && <p> Wrong!</p>}
+      {lastRightWrong===true && <p> Correct </p>}
+      {lastRightWrong===false && <p> Wrong!</p>}
 
       <button onClick={resetTranscript}>Reset</button>
       <span>{transcript}</span>
       <p> Say the country</p>
-      <p>🇧🇷</p>
+      <p>{selectionCountryEmoji}</p>
     </div>
   );
 };
