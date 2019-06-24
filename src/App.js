383d6a1cd7d6663eab1479d8ddf69411a2d36ac4
christopher.hogg <christopher.hogg@blik.io>
2019-06-24 12:20:16 +0100

    feat: making it look good

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import SpeechRecognition from "react-speech-recognition";
-import MyStopwatch from "./Stopwatch";
 import { countries } from "./countries";
 import "./App.sass";
 const startState = "start";
 const playingState = "play";
-
+const passWord = 'pass'
 const propTypes = {
   // Props injected by SpeechRecognition
   transcript: PropTypes.string,
@@ -35,9 +34,16 @@ const Dictaphone = ({
     resetTranscript();
     setLastCorrectAnswer(selectedCountryName);
   };
+
+  const skippedAnswerActions = () => {
+    setLastRightWrong(false);
+    setCountryNumber(countryNumber + 1);
+    resetTranscript();
+  };
   const CentreColumn = () => {
   if (playState === startState) {
     if (transcript.includes("start")) {
+      resetTranscript();
       setPlayState(playingState);
     }
     return (
@@ -52,13 +58,16 @@ const Dictaphone = ({
 
   if (countryNumber === numberOfCountries - 1) {
     if (transcript.includes("home")) {
+      setLastCorrectAnswer('')
+      resetTranscript();
       setPlayState(startState);
       setCountryNumber(0);
     }
     return (
       <div>
-        {" "}
-        <p> End of quiz say home to begin</p>{" "}
+                <p className={"is-size-2"}> End of quiz</p>{" "}
+
+        <p className={"is-size-1 has-text-weight-bold"}> Say home to begin</p>{" "}
         <button
           onClick={() => {
             setPlayState(startState);
@@ -85,20 +94,19 @@ const Dictaphone = ({
       correctAnswerActions();
     }
 
+    if (transcript.includes(passWord)) {
+      skippedAnswerActions();
+    }
+
     return (
       <div className={"is-size-2"}>
               <p className={'has-text-weight-bold'}> Say the country</p>
 
               <span className={"is-size-6"}>{selectedCountryEmoji}</span>
-
-        <MyStopwatch />
-        {lastRightWrong === true && <p> Correct </p>}
-        {lastRightWrong === false && <p> Wrong</p>}
-
-        <button onClick={resetTranscript}>Reset</button>
-        <span>{transcript}</span>
-        <p> Last correct answer </p>
+        <p> Last correct answer: </p>
         <p> {lastCorrectAnswer} </p>
+        <span className={'is-size-5'}>{transcript}</span>
+
       </div>
     );
   }
